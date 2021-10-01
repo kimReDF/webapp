1fe12891b2705eff85ef66ee6b29ed36540754b4
kimReDF <87419917+kimReDF@users.noreply.github.com>
2021-10-01 14:06:03 +0200

    Update app.py

--- app.py
+++ app.py
@@ -27,4 +27,4 @@ app.layout = html.Div(children=[
 ])
 
 if __name__ == '__main__':
-    application.run(debug=True, host='0.0.0.0', port='8000')
+    application.run(debug=True, host='0.0.0.0', port='8080')
